fab616440abeccf040cc267e36ede88f77b7e3a8
Akshaya761 <akshayarodda583@gmail.com>
2026-02-11 15:27:54 +0530

    Add files via upload

---
+++ datefaction.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main() {
+int you, date;
+    printf("enter the rating of you and your date ");
+    scanf("%d%d",&you,&date);
+    if( you<=2|| date<=2)
+    {
+      printf("0");
+    }
+    else if( you >=8|| date>=2)
+    {
+    printf("2");
+}
+else
+{
+ printf("1");
+      }
+    return 0;
+} 
